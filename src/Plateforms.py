5bcac6c8defc4d4878d05812022b3a921fb8a36a
dfayle <dfayle@binghamton.edu>
2023-11-22 12:06:23 -0500

    edits

--- src/Plateforms.py
+++ src/Plateforms.py
@@ -1,2 +1,10 @@
-class Plateforms:
-    def __init__(self):
+import pygame
+class Plateform(pygame.sprite.Sprite):
+    def __init__ (self x,y) :
+        super().__init()
+        self.width = 70
+        self.height = 20
+        self.image = pygame.Surface((self.width, self.height))
+        self.image.fill("White")
+        self
+
