ee27096aa62d523c821c34d81c6c6eb338b129ea
dfayle <dfayle@binghamton.edu>
2023-11-24 12:17:05 -0500

    commit

--- src/Doodle.py
+++ src/Doodle.py
@@ -1,9 +1,14 @@
+import pygame
 class Doodle:
     def __init__(self, image, flip, x, y):
         self.image = image
         self.flip = flip
         self.x = x
         self.y = y
+        self.width = width
+        self.height = height
+        self.image = pygame.image.load(image_path)
+        self.image = pygame.transform.scale(self.image, (width, height))
         
     def flip_direction(self, left):
         self.flip = pygame.image.load("flipped image name")
