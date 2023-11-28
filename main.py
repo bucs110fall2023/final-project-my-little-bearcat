0dcfb8287c4346142896ee3e8bb9c7d2f42c7176
dfayle <dfayle@binghamton.edu>
2023-11-28 14:25:04 -0500

    commit

--- main.py
+++ main.py
@@ -9,7 +9,5 @@ def main():
     #Call your mainloop
     
     ###### NOTHING ELSE SHOULD GO IN main(), JUST THE ABOVE 3 LINES OF CODE ######
-
-# https://codefather.tech/blog/if-name-main-python/
 if __name__ == '__main__':
     main()
